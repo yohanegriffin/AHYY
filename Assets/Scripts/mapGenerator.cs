1cf6c5d0d28ee544437bcfed4affe6145ad5551b
HunterWebb <hjwebb@knox.edu>
2023-01-23 14:19:06 -0600

    big explosion with oneDrive hopefully fixed

---
+++ Assets/Scripts/mapGenerator.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mapGenerator : MonoBehaviour
+{
+    public GameObject mapTile;
+
+    [SerializeField] private int mapWidth;
+    [SerializeField] private int mapHeight;
+
+    private void Start()
+    {
+        generateMap();
+    }
+
+    private void generateMap()
+    {
+        for(int y = 0; y < mapHeight; y++)
+        {
+            for(int x = 0; x < mapWidth; x++)
+            {
+                GameObject newTile = Instantiate(mapTile);
+
+                newTile.transform.position = new Vector2(x, y);
+            }
+        }
+    }
+}
