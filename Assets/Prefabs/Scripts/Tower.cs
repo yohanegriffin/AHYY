6af1ebd43df0a4573a81eb151446375fd6daf2d5
HunterWebb <hjwebb@knox.edu>
2023-01-23 13:25:16 -0600

    Working tower script done, need to have enemy and map scripts done to complete

---
+++ Assets/Prefabs/Scripts/Tower.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+    [SerializeField] private float range;
+    [SerializeField] private float damage;
+    [SerializeField] private float attackTime; // Time in seconds between shots
+
+    private float nextTimeToShoot;
+
+    private GameObject currentTarget;
+
+    private void Start()
+    {
+        nextTimeToShoot = Time.time;
+    }
+
+    private void updateNearestEnemy()
+    {
+        GameObject currentNearestEnemy;
+        float distance = Mathf.Infinity;
+        foreach (GameObject enemy in Enemies.enemies)
+        {
+            float _distance = (transform.position - enemy.transform.position).magnitude;
+
+            if(_distance < distance)
+            {
+                distance = _distance;
+                currentNearestEnemy = enemy;
+            }
+        }
+        if(distance <= range)
+        {
+            currentTarget = currentNearestEnemy;
+        }
+        else
+        {
+            currentTarget = null;
+        }
+    }
+
+    private void shoot()
+    {
+        
+            Enemy enemyScript = currentTarget.GetComponent<Enemy>();
+
+            enemyScript.takeDamage(damage);
+    }
+
+    private void Update()
+    {
+        updateNearestEnemy();
+
+        if(Time.time >= nextTimeToShoot)
+        {
+            if(currentTarget != null)
+            {
+                shoot();
+                nextTimeToShoot = Time.time + attackTime;
+            }
+        }
+    }
+
+
+}
