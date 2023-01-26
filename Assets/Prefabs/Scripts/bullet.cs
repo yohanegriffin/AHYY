6992658e347cae4dac3732d46064efdff037f2b0
HunterWebb <hjwebb@knox.edu>
2023-01-26 16:37:40 -0600

    added bullets destroying on hit of enemy

--- Assets/Prefabs/Scripts/bullet.cs
+++ Assets/Prefabs/Scripts/bullet.cs
@@ -4,6 +4,17 @@ using UnityEngine;
 
 public class bullet : MonoBehaviour
 {
+    private void Start()
+    {
+        Destroy(gameObject, 10f);
+    }
+    
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(gameObject);
+    }
+
+    
     private void Update()
     {
         transform.position += transform.right * 0.1f;
