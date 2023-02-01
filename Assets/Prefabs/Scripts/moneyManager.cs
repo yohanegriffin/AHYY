c15e1b9d941289b4d20de94d71ea4ae6c46aae37
HunterWebb <hjwebb@knox.edu>
2023-02-01 14:27:46 -0600

    Changed values for enemies and kill rewards

--- Assets/Prefabs/Scripts/moneyManager.cs
+++ Assets/Prefabs/Scripts/moneyManager.cs
@@ -9,7 +9,7 @@ public class moneyManager : MonoBehaviour
 
     public void Start()
     {
-        starterMoney = 100;
+        starterMoney = 50;
         this.currentPlayerMoney = starterMoney;
         Debug.Log("Current Money " + getCurrentMoney());
     }
